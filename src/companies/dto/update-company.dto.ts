9e11d085485369d6a021ac655ace12ed5522a098
Kady <145310337+KirinKoji@users.noreply.github.com>
2024-08-08 16:54:56 +0700

    Delete src directory

--- src/companies/dto/update-company.dto.ts
+++
@@ -1,4 +0,0 @@
-import { CreateCompanyDto } from "./create-company.dto";
-import { PartialType } from "@nestjs/mapped-types";
-
-export class UpdateCompanyDto extends PartialType(CreateCompanyDto) {}
